aae0bf7669251adcd78b04d7c6bb35722fb49fa1
johadi <jim.hadi10@gmail.com>
2017-05-08 10:56:58 +0100

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ILYASANATE on 07/05/2017.
+ */
+import '../build/css/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App=()=>{
+    return(
+        <div>Hello from hadi</div>
+    );
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'));
